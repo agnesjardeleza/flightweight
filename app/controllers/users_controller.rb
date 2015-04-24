406a27b82119fbc758cdc82d04bbe76c3c946fb3
Cjay <billonesciprian08@gmail.com>
2015-04-25 00:27:43 +0800

    Make User Registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,21 @@ class UsersController < ApplicationController
 	end
 
 	def create
-		render plain: params[:user].inspect
+		@user = User.new(user_params)
+
+		if @user.save
+			@person = Person.new(person_params)
+			if @person.save
+				flash[:notice] = "Successfully Created Account!"
+				redirect_to :login_users_path
+			else
+				flash.now[:error] = @person.errors.full_messages.first
+				render 'users/new'
+			end
+		else
+			flash.now[:error] = @user.errors.full_messages.first
+			render 'users/new'
+		end
 	end
 
 	def edit
@@ -19,5 +33,14 @@ class UsersController < ApplicationController
 	def destroy
 		
 	end
+
+	private
+		def user_params
+			params.require(:user).permit(:username, :password, :password_confirmation, :role)
+		end
+
+		def person_params
+			params.require(:person).permit(:first_name, :last_name, :mid_name, :address, :contact_num, :email).merge(:user_id => @user.id)
+		end
 
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,10 @@
 class Person < ActiveRecord::Base
+
+	validates_presence_of :first_name
+	validates_presence_of :last_name
+	validates_presence_of :mid_name
+	validates_presence_of :address
+	validates_presence_of :contact_num
+	validates_presence_of :email
+
 end
