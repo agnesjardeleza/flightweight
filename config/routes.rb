5ffe1fc27cdf8405a9992760551de702dea24eb2
Cjay <billonesciprian08@gmail.com>
2015-04-24 22:56:39 +0800

    Generate Models

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,14 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
    root 'static_pages#home'
 
+   resources :users
+   resources :people
+   resources :comments
+   resources :posts
+   resources :transactions
+   resources :categories
+   resources :bids
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
