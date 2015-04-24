a740af408e24f9136a3a4be3e7e6b91e6b1e47c0
Genesis Palaganas <itetsky123@gmail.com>
2015-04-25 00:26:22 +0800

    Adds login view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,4 +20,31 @@ class UsersController < ApplicationController
 		
 	end
 
+  def login
+    
+    if current_user == nil
+      @user = User.new
+    end
+
+  end
+
+  def sign_in
+    
+    username = params[:user][:username]
+    password = params[:user][:password]
+
+    user = User.authenticate(username, password)
+
+    if user
+      session[:user_id] = user.id
+      @user = (current_user.role == 'admin')? User.where(user_id:current_user.id).first : nil
+      redirect_to :root
+    else
+      flash[:error] = "Invalid username or password."
+      redirect_to sign_in_users_path
+    end
+
+
+  end
+
 end
